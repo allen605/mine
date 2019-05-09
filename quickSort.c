95b18f4b962d7319b3874b4cfcfe5ebe44cb0c61
allen <xionghuihui@actions-micro.com>
2019-05-09 17:45:56 +0800

    Add quick sort func.

---
+++ quickSort.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+快速排序（交换类）：主要分两个步骤，
+1. 先来一次快排：将第一个元素作为枢轴，从右往左遍历，碰到比key小的，则换位置；换完后再从左往右遍历，碰到比key大的，则换位置。
+  时间复杂度为n
+
+2. 经过一次快排后，继续对小于key的和大于key的部分分别进行递归快排。
+
+*/
+int LEN;
+
+/*打印指定整形数组*/
+void print_arr(int *array, int L, int R){
+    if(L<R){
+        while(L <= R){
+            printf("%-3d", array[L++]);
+        }
+    }
+    printf("\n");
+}
+
+void quickSort(int *arr, int left,int right){
+/*  printf("start->L=%d R=%d ",left, right);
+    print_arr(arr, left, right);
+*/
+    if(left < right){  //从递归退出的条件
+        int L = left;
+        int R = right;
+        int key = arr[L];
+
+        while(L!=R){
+            /*先从右往左遍历，直到遇到比key小的*/
+            while(arr[R] >= key && L<R){
+                R--;
+            }
+            if(R>L){
+                arr[L] = arr[R];
+                L++;
+            }
+            /*再从左往右遍历，直到遇到比key大的*/
+            while(arr[L] < key && L<R){
+                L++;
+            }
+            if(L<R){
+                arr[R] = arr[L];                
+                R++;
+            }
+        }
+        arr[L]=key;//经过一次快排之后，原来的序列变成以key为中枢，划分为两部分（左边的都小于key，右边的大于key）
+        /*
+        printf("end->key=[%d] ",L);
+        print_arr(arr,left,right);
+        printf("\n");
+        */
+        quickSort(arr, left, L-1);
+        quickSort(arr, L+1, right);
+    }
+}
+
+int main()
+{
+    int arr[]={49, 38, 65, 97, 76, 13, 27, 49};
+    int length=sizeof(arr)/sizeof(int);
+    LEN=length;
+
+    quickSort(arr, 0, LEN-1);
+
+    print_arr(arr, 0, LEN-1);
+    exit(0);
+}
